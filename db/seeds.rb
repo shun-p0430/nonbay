a97a20e02bc3d8801e01fdca0eb229c16cc404a9
shun-p0430 <chocosensei@gmail.com>
2022-04-22 16:51:32 +0900

    初期投入データ修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,16 +2,26 @@ require 'net/http'
 require 'uri'
 require 'json'
 
-brandsUri = URI.parse('https://muro.sakenowa.com/sakenowa-data/api/brands')
-brandsJson = Net::HTTP.get(brandsUri)
-brands = JSON.parse(brandsJson)
-brands["brands"].each do |data|
-  Brand.create(name: data['name'], brewery_id: data['breweryId'])
-end
-
 breweriesUri = URI.parse('https://muro.sakenowa.com/sakenowa-data/api/breweries')
 breweriesJson = Net::HTTP.get(breweriesUri)
 breweries = JSON.parse(breweriesJson)
-breweries["breweries"].each do |data|
-  Brewery.create(name: data['name'], area_id: data['areaId'])
+breweries["breweries"].each do |brewery|
+  begin
+    data = Brewery.find_or_create_by(id: brewery['id'])
+    data.update(id: brewery['id'], name: brewery['name'], area_id: brewery['areaId'])
+  rescue => e
+    Rails.logger.debug e.message
+  end
 end
+
+# brandsUri = URI.parse('https://muro.sakenowa.com/sakenowa-data/api/brands')
+# brandsJson = Net::HTTP.get(brandsUri)
+# brands = JSON.parse(brandsJson)
+# brands["brands"].each do |brand|
+#   begin
+#     data = Brand.find_or_create_by(id: brand['id'])
+#     data.update(id: brand['id'], name: brand['name'], brewery_id: brand['breweryId'])
+#   rescue => e
+#     Rails.logger.debug e.message
+#   end
+# end
