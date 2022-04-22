d33027337cffd8fe1bbeb9679e368e17c47d3d18
shun-p0430 <chocosensei@gmail.com>
2022-04-22 14:36:32 +0900

    初期データ投入

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
+require 'net/http'
 require 'uri'
 require 'json'
 
-uri = URI.parse('https://muro.sakenowa.com/sakenowa-data/api/brands')
-json = Net::HTTP.get(uri)
-arr = JSON.parse(json[0][:brands])
-arr.each do |data|
-  Brand.create(column1:data['name'], column2:data['breweryId'])
+brandsUri = URI.parse('https://muro.sakenowa.com/sakenowa-data/api/brands')
+brandsJson = Net::HTTP.get(brandsUri)
+brands = JSON.parse(brandsJson)
+brands["brands"].each do |data|
+  Brand.create(name: data['name'], brewery_id: data['breweryId'])
+end
+
+breweriesUri = URI.parse('https://muro.sakenowa.com/sakenowa-data/api/breweries')
+breweriesJson = Net::HTTP.get(breweriesUri)
+breweries = JSON.parse(breweriesJson)
+breweries["breweries"].each do |data|
+  Brewery.create(name: data['name'], area_id: data['areaId'])
 end
